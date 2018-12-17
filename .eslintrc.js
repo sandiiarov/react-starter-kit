baaa08580e11407bf55c30666e696ebf4ece5c49
Alex Sandiiarov <sandiyarov@gmail.com>
2018-12-17 14:47:57 -0600

    upd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,14 @@
 module.exports = {
   env: {
-      es6: true,
-      browser: true,
-      node: true,
-      jest: true
+    es6: true,
+    browser: true,
+    node: true,
   },
   parser: 'babel-eslint',
   extends: ['airbnb', 'plugin:prettier/recommended'],
   rules: {
-      'prettier/prettier': [
-          'error', { trailingComma: 'es5', singleQuote: true }
-      ],
-      'react/jsx-filename-extension': [ 'error', { extensions: ['.js'] }
-      ]
-  }
+    'prettier/prettier': ['error', { trailingComma: 'es5', singleQuote: true }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+    'react/jsx-one-expression-per-line': 'off',
+  },
 };
--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
 
-const App = () => (
-  <div>
-    <h1>Hello World!</h1>
-  </div>
-);
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        +
+      </button>
+    </div>
+  );
+};
 
 export default hot(module)(App);
